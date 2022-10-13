d7f2203dc1f5ded316190fc81ab54bc01e5b8254
Pedro Grando <grandop03@gmail.com>
2022-10-13 16:36:21 -0300

    Adicionando as questões teoricas

---
+++ Ex-OO-part2.playground/Pages/Ex3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class Pai {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    private func printName() {
+        
+    }
+}
+
+class Filho: Pai {
+    
+    override func printName() {
+        
+    }
+}
+//: [Next](@next)
---
+++ Ex-OO-part2.playground/Pages/WritingExercises.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+/*
+Ex 1
+Esconder detalhes, muito parecido com a abstração.
+--------------------------------------------------------
+Ex 2
+Public, open, internal, fileprovate, private. O padrao é o internal. Focaremos no internal e no private.
+--------------------------------------------------------
+Ex 4
+Um jeito de esconder aquilo que o usuário não precisa saber, mas ele sabe que acontece
+--------------------------------------------------------
+Ex 5
+Quando uma classe herda os atributos da super classe
+--------------------------------------------------------
+Ex 6
+Fazer vários métodos com a mesma função, mas escritas de formas diferentes, o que escrevendo uma já é o suficiente
+--------------------------------------------------------
+Ex 9
+Quando tu quer chamar um atributo da super classe (super.exemplo)
+--------------------------------------------------------
+Ex 10
+colocar ( Private method() {} ) na super classe
+--------------------------------------------------------
+
+ */
+
+
+
+//: [Next](@next)
